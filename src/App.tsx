f53aa8664c8a3dcca5fcbb29420eecae895e0d5f
Toluddev <toluwalaseoludipe7@gmail.com>
2025-06-18 02:39:15 +0100

    sections updated

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
-import Products from "./pages/Products";
-import Partners from "./pages/Partners";
+
 import Contact from "./pages/Contact";
 
 import Photojournalism from "./pages/Photojournalism";
