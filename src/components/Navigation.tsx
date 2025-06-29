e6d17ced69a0088569ead15ddd0515c893d78d8c
Toluddev <toluwalaseoludipe7@gmail.com>
2025-06-29 22:48:32 +0100

    logo updated

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -31,7 +31,6 @@ const Navigation = () => {
             <div className="flex items-center space-x-2 p-2">
                 <Link to="/" className="text-white">
                     <img src="/lovable-uploads/Conjugal Vows_2.jpg.png" alt="Logo" className="h-20 w-25" />
-
                 </Link>
             </div>
             {/* Navigation Pills - Hidden on mobile */}
